6b77845392d3bf2d7aae5b0f271f0d2caa75505c
rhythm <rhythmkc7@gmail.com>
2022-06-13 13:15:50 -0400

    minor fix

--- backend/api/database/mongoose.js
+++ backend/api/database/mongoose.js
@@ -2,7 +2,6 @@ const env = require('dotenv')
 env.config()
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-console.log(process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI).then(console.log('connected to the database'))
 .catch((error)=>console.log(error));
 
