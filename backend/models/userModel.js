cfea8f27cc8bf270c2143b05e1decb6902e637de
rhythm <rhythmkc7@gmail.com>
2022-07-02 15:55:50 -0400

    created user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true
+    },
+    Passowrd:{
+        type:String,
+        required:true
+    },
+    Role:{
+        type:String,
+        required:true
+    }
+
+})
