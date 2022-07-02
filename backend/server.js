30dd98b0bd0fee986847cbc527c07b8308c6a8dc
rhythm <rhythmkc7@gmail.com>
2022-07-02 13:20:32 -0400

    allowed accesss controle

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use("/buildings", require("./routes/buildingRoutes.js"))
 app.use("/analytics", require("./routes/analyticsRouter"))
