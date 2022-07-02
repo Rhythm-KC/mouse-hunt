023c7d5c2e069aa783653be783d9d0b898359e79
rhythm <rhythmkc7@gmail.com>
2022-07-01 21:36:09 -0400

    MVVC model

--- backend/api/database/mongoose.js
+++
@@ -1,8 +0,0 @@
-const env = require('dotenv')
-env.config()
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI).then(console.log('connected to the database'))
-.catch((error)=>console.log(error));
-
-module.exports = mongoose;
