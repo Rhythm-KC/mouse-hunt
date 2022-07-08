e9b5efb8cb649dbf8ffe053e91dd8c676bba5d70
rhythm <rhythmkc7@gmail.com>
2022-07-08 19:21:54 -0400

    added authentication and guards to api's so only a vaild user can access the resources

--- ui/mouse-hunt/src/app/components/login/login.component.ts
+++ ui/mouse-hunt/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  invalid:boolean
+  submitted:boolean
+  loginForm:FormGroup
+  constructor(private router:Router, private auth:AuthService, private fb :FormBuilder) {
+    this.loginForm= this.buildForm()
+    this.invalid = false
+    this.submitted = false
+  }
   ngOnInit(): void {
+    this.loginForm= this.buildForm()
   }
 
+
+  login(){
+    this.submitted=true
+    if(this.loginForm.invalid ===true){
+      return
+    }
+
+    this.auth.loginUser(this.loginForm.value['userName'], this.loginForm.value["Password"]).subscribe(
+      {
+        next:()=>{
+          console.log("redirect home")
+          this.router.navigate(["/home"])
+        },
+        error:()=>{
+          this.invalid = true
+        }
+      }
+    )
+  }
+
+  get f(){
+    return this.loginForm.controls
+  }
+
+  buildForm():FormGroup{
+    return this.fb.group({
+      userName:["", Validators.required],
+      Password:["", Validators.required]
+    })
+  }
 }
